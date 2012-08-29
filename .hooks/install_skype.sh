ca48ca970be678ada77d1802c3d3321fc6aa68a2
Simon Schiele <simon.schiele@ypsilon.net>
2012-08-29 16:24:31 +0200

    install_skype hook: manual install

--- .hooks/install_skype.sh
+++ .hooks/install_skype.sh
@@ -4,12 +4,15 @@ hook_systemtypes="workstation laptop"
 hook_optional=false
 hook_version=0.1
 
-echo ">>> Generating '/etc/apt/sources.list.d/skype.list'"
-echo -e "deb http://download.skype.com/linux/repos/debian/ testing non-free" > /etc/apt/sources.list.d/skype.list
+#echo ">>> Generating '/etc/apt/sources.list.d/skype.list'"
+#echo -e "deb http://download.skype.com/linux/repos/debian/ testing non-free" > /etc/apt/sources.list.d/skype.list
 
-echo ">>> Updating package lists"
-apt-get update
+#echo ">>> Updating package lists"
+#apt-get update
 
-echo ">>> Install package 'skype'"
-apt-get install skype
+#echo ">>> Install package 'skype'"
+#apt-get install skype
+
+wget -O skype-install.deb http://www.skype.com/go/getskype-linux-deb
+dpkg -i skype-install.deb
 
