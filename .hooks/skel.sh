2c34c29a8f2072a714ae095fe3c78c9ce3b3f57a
Simon Schiele <simon.schiele@ypsilon.net>
2013-07-10 23:38:22 +0200

    hooks/skel: commented config values

--- .hooks/skel.sh
+++ .hooks/skel.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 hook_name=skel
-hook_systemtypes="minimal server workstation laptop"
-hook_optional=false
-hook_version=0.1
-hook_once=true
-hook_sudo=true
+hook_systemtypes="minimal server workstation laptop"    # optional, default: empty
+hook_optional=false                                     # optional, default: true
+hook_version=0.1                                        # optional, default: 0.0
+hook_once=true                                          # optional, default: true
+hook_sudo=true                                          # optional, default: true
 [ -r ~/.hooks/helper.sh ] && . ~/.hooks/helper.sh || ( echo "ERROR: '~/.hooks/helper.sh' not found" ; exit 1 )
 ###########################################################
