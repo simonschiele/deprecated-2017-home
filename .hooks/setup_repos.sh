c6d4ebd5f009f89240a34a56f3564eb6c6bc8c59
Simon Schiele <simon.schiele@ypsilon.net>
2013-11-26 22:33:14 +0100

    time to use the hooks again :-)

---
+++ .hooks/setup_repos.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+hook_name=setup_repos
+hook_systemtypes="minimal server workstation laptop"
+hook_optional=false
+hook_once=false
+hook_version=0.1
+hook_sudo=false
+#hook_hostnames=""
+[ -r ~/.hooks/helper.sh ] && . ~/.hooks/helper.sh || ( echo "ERROR: '~/.hooks/helper.sh' not found" ; exit 1 )
+###########################################################
+
+status=0
+
+#echo "Fixing home repo"
+#sed -i -e 's|http.*://simon.psaux.de/git/|git@simon.psaux.de:|g' \
+#/home/simon/.git/config
+echo "status: $status"
+if [ ${status} == 0 ]
+then
+    echo "> Checking repos"
+    cd ~/
+    cd "${OLDPWD}" 2>/dev/null
+else
+    echo "> Skipped checking repos."
+fi
+
+if [ ${status} == 0 ]
+then
+    echo "> Checking private repos"
+    cd ~/
+    
+    if [ ! -d .bin-private/ ]
+    then
+        echo -e "> Checking out ~/.bin-private/"
+        git clone git@psaux.de:dot.bin-private.git .bin-private
+    fi
+    
+    if [ ! -d .bin-private/ ]
+    then
+        echo -e "> Checking out ~/.bin-private/"
+        git clone git@psaux.de:dot.bin-ypsilon.git .bin-ypsilon
+    fi
+    
+    if [ ! -d .fonts/ ] || [ ! -d .fonts/.git/ ] 
+    then
+        if [ -d .fonts/ ]
+        then
+            echo -e "> Moving old .fonts folder -> ~/.fonts-backup/"
+            mv .fonts .fonts-backup
+            
+            echo -e "> Checking out ~/.fonts/"
+            git clone git@psaux.de:dot.fonts.git .fonts/
+        fi
+    fi
+    
+    if [ ! -d .backgrounds/ ] || [ ! -d .backgrounds/.git/ ] 
+    then
+        if [ -d .backgrounds/ ]
+        then
+            echo -e "> Moving old .backgrounds folder -> ~/.backgrounds-backup/"
+            mv .backgrounds .backgrounds-backup
+            
+            echo -e "> Checking out ~/.backgrounds/"
+            git clone git@psaux.de:dot.backgrounds.git .backgrounds/
+        fi
+    fi
+   
+
+    cd "${OLDPWD}" 2>/dev/null
+else
+    echo "> Skipped checking private repos."
+fi
+
--- .hooks/setup_vim.sh
+++ .hooks/setup_vim.sh
@@ -2,21 +2,64 @@
 hook_name=setup_vim
 hook_systemtypes="minimal server workstation laptop"
 hook_optional=false
+hook_once=false
 hook_version=0.4
+hook_sudo=false
 [ -r ~/.hooks/helper.sh ] && . ~/.hooks/helper.sh || ( echo "ERROR: '~/.hooks/helper.sh' not found" ; exit 1 )
 ###########################################################
 
 status=0
 
-cd ~/.vim/
-git submodule init
-git submodule update --recursive
-cd $OLDPWD
-
-if ! [ -d ~/.vim/ ] || ! [ -e ~/.vim/.git ]
+if ! [ -d .vim/ ] || ! [ -e .vim/.git ]
 then
-    echo "vim config not checked out properly"
+    echo "> vim config not checked out properly. canceling."
     status=1
+fi
+
+if [ ${status} == 0 ]
+then
+    echo "> Checking for submodules"
+    cd .vim/
+    
+    if [ ! -e plugins/vim-pathogen/autoload/pathogen.vim ]
+    then
+        echo "> No submodules found. Checking out."
+        git submodule init || status=1
+        git submodule update --recursive || status=1
+    fi
+    
+    cd "${OLDPWD}" 2>/dev/null
+else
+    echo "> Skipped submodules test"
+fi
+
+if [ ${status} == 0 ]
+then
+    echo "> Checking submodules for updates"
+    cd .vim/plugins/
+    
+    echo -e "\t* Update in powerline-fonts"
+    echo -e "\t* Update in lib/git-prompt"
+    
+    cd "${OLDPWD}" 2>/dev/null
+else
+    echo "> Skipped checking submodules for updates"
+fi
+
+if [ ${status} == 0 ]
+then
+    echo "> Checking vimproc"
+    cd ~/.vim/plugins/vimproc/
+    
+    if [ ! -e autoload/vimproc_unix.so ]
+    then
+        echo "> No binary found. Setting up vimproc."
+        make -f make_unix.mak
+    fi
+    
+    cd "${OLDPWD}" 2>/dev/null
+else
+    echo "> Skipped checking submodules for updates"
 fi
 
 #echo "Setting up Command-T"
@@ -25,8 +68,4 @@ fi
 #make
 #cd $OLDPWD
 
-echo "Setting up vimproc"
-cd ~/.vim/plugins/vimproc/
-make -f make_unix.mak
-cd $OLDPWD
 
