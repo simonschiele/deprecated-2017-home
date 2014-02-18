66ecaa2c62b742e42fcf448e9f6738f62f8c7292
Simon Schiele <simon.schiele@ypsilon.net>
2014-02-18 22:38:00 +0100

    fixed hooks: setup_gobi2000-umts and setup_share

--- .hooks/setup_gobi2000-umts.sh
+++ .hooks/setup_gobi2000-umts.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 hook_name=setup_gobi2000-umts
 hook_systemtypes="laptop"
-hook_optional=true
-hook_version=0.0
+hook_optional=false
+hook_version=0.3
+hook_hostnames="cpad"
 [ -r ~/.hooks/helper.sh ] && . ~/.hooks/helper.sh || ( echo "ERROR: '~/.hooks/helper.sh' not found" ; exit 1 )
 ###########################################################
 
-mkdir modem/
-cd modem/
+if ( ! rm -rf /unpack-modem/modem 2>/dev/null ) ; then
+    echo "> ERROR: Could not remove old '/unpack-modem/modem' build-folder"
+    exit 1
+fi
 
-apt-get install gobi-loader
+mkdir -p /tmp/unpack-modem/
+cd /tmp/unpack-modem/
 
-wget http://metashell.de/mirror/arch/aur/gobi-firmware/gobi-firmware.zip
-unzip gobi-firmware.zip
+if ( ! is_installed gobi-loader ) ; then
+    sudo apt-get install gobi-loader
+fi
 
-mkdir -p /lib/firmware/gobi/
+if [[ -e /share/Software/Firmware/gobi-firmware.tar.gz ]] && ( tar xzf /share/Software/Firmware/gobi-firmware.tar.gz ) ; then
+    echo "Successfully unpacked from /share/Software/Firmware/gobi-firmware.tar.gz"
+    sudo mkdir -p /lib/firmware/gobi/
+    sudo cp amss.mbn apps.mbn UQCN.mbn /lib/firmware/gobi/
+elif ( wget http://metashell.de/mirror/arch/aur/gobi-firmware/gobi-firmware.zip ) && ( unzip gobi-firmware.zip ) ; then
+    echo "Successfully downloaded and unpacked from http://metashell.de/mirror/arch/aur/gobi-firmware/gobi-firmware.zip"
+    sudo mkdir -p /lib/firmware/gobi/
+    sudo cp UMTS/amss.mbn /lib/firmware/gobi/
+    sudo cp UMTS/apps.mbn /lib/firmware/gobi/
+    sudo cp 6/UQCN.mbn /lib/firmware/gobi/
+else
+    echo "> ERROR: Could not download (or unpack) gobi-firmware.zip"
+    success=false
+fi
 
-cp UMTS/amss.mbn /lib/firmware/gobi/
-cp UMTS/apps.mbn /lib/firmware/gobi/
-cp 6/UQCN.mbn /lib/firmware/gobi/
+cd $OPWD
 
--- .hooks/setup_share.sh
+++ .hooks/setup_share.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 hook_name=setup_share
-hook_systemtypes="minimal server workstation laptop"
-hook_optional=true
-hook_version=0.0
+hook_systemtypes="workstation laptop mediacenter"
+hook_optional=false
+hook_version=0.3
 [ -r ~/.hooks/helper.sh ] && . ~/.hooks/helper.sh || ( echo "ERROR: '~/.hooks/helper.sh' not found" ; exit 1 )
 ###########################################################
 
-if [ ! -d /share/ ]
-then
-    mkdir /share/
+sudo mkdir -p /share/
+sudo chmod 777 /share/
+
+if ( ! grep -q -e "//192.168.5.102/share" -e "//cbase/share" -e "//cbase.cnet/share" /etc/fstab ) ; then
+    echo "> Adding /share/ to fstab"
+    echo -e "//cbase.cnet/share\t/share\tcifs\tdefaults,username=simon,password=SigiIstDoof\t0 0" | sudo tee -a /etc/fstab >/dev/null
 fi
 
-if (!( grep -q "//192.168.5.102/share" /etc/fstab ) && !( grep -q "//cbase/share" /etc/fstab ))
+if ( fping -q cbase.cnet )
 then
-    echo -e "//192.168.5.102/share\t/share\tcifs\tdefault,username=simon,password=SigiIstDoof\t0 0" >> ${BOOTSTRAP_MOUNT}/etc/fstab
-fi
-
-if ( fping -q 192.168.5.102 )
-then
-    mount /share/
+    echo "> Mounting /share/"
+    sudo mount /share/
 fi
 
